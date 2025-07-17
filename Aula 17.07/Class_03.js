c9285c8daa756d26e66f0219c4299d76b19cdde2
Pereira <isabela.pereira@sap.com>
2025-07-17 15:40:48 -0300

    aula 4 - add exercicio

---
+++ Aula 17.07/Class_03.js
@@ -0,0 +1,17 @@
+let idade = 25  ;     //number
+let nome  = 'Isabela' ;   // string
+let ativo = true ;    //boolean
+let nota ;            // undefined 
+let cidade = null ;   // null
+
+
+console.log("idade:", idade);
+console.log("nome:", nome);
+console.log("ativo:", ativo);
+console.log("nota:", nota);
+console.log("cidade:", cidade);
+
+const primeiroNum = 5;
+const segundoNum = 10; 
+const resultado = primeiroNum + segundoNum; 
+const resultadoDuplicado = resultado*2;
