2d1188c879da98685bdcceebccf424c4c6c4f076
Pereira <isabela.pereira@sap.com>
2025-08-25 14:28:03 -0300

    aula 3

--- Aula 3/exercicios_aula_3.js
+++ Aula 3/exercicios_aula_3.js
@@ -15,3 +15,26 @@ const primeiroNum = 5;
 const segundoNum = 10; 
 const resultado = primeiroNum + segundoNum; 
 const resultadoDuplicado = resultado*2;
+
+// EXERCÍCIOS
+
+//Luiz Otavio Miranda tem 30 anos, pesa 84kg
+//tem 1.8 de altura e seu IMC é de 25.9259....
+//luiz miranda nasceu em 1980
+
+const nome = "Luiz Otavio Miranda"
+let idade = 30
+let anoAtual = 2010
+let nascimento = anoAtual - idade
+let peso = 84
+let altura = 1.8
+let IMC = peso / (altura*altura)
+
+//Luiz Otavio Miranda tem 30 anos, pesa 84kg 
+console.log(nome, "tem", idade,"anos, pesa ", peso, "kg" )
+console.log(`${nome} tem ${idade} anos, pesa ${peso} kg`)
+
+//tem 1.8 de altura e seu IMC é de 25.9259....
+console.log("tem", altura, "de altura e seu IMC é de ", IMC)
+
+// luiz miranda nasceu em 1980
